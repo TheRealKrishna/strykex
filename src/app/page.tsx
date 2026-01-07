ac7479e281c89faffc6d2cdedff3c80bbded20f8
TheRealKrishnam <agarwl.krishna2@gmail.com>
2026-01-07 18:44:54 +0530

    feat: implement redirect to dashboard in next.config.ts and remove useEffect from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
-import { useEffect } from "react";
-
 export default function Home() {
-  useEffect(() => {
-    window.location.href = "/dashboard";
-  }, []);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
   );
